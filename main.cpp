f667b55aeedb5ac10116667861c59e0f7d9f5a61
jalonsoalonso <jesus.alonso@gmail.com>
2020-02-20 15:42:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
+//Comment next line to use C version or uncomment for C++ version
+#define CPLUSPLUS
+#ifdef CPLUSPLUS
 
 #include <iostream>
-
-int main(int argc, char** argv) {
-	std::cout << "Have " << argc << " arguments:" << std::endl;
-	for (int i = 0; i < argc; ++i) {
+int main(int argc, char *argv[])
+{
+	std::cout << "There are " << argc << " arguments:" << std::endl;
+	for (int i = 0; i < argc; ++i)
+	{
 		std::cout << argv[i] << std::endl;
 	}
 }
+
+#else
+
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	printf("There are %d arguments:\n", argc);
+	for (int i = 0; i < argc; ++i)
+	{
+		printf("%s\n", argv[i]);
+	}
+}
+
+#endif
